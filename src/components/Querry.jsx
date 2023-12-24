807986a39b43c6b1ab17c904139f03fe1693bc33
Karthick2816 <rajakarthick924@gmail.com>
2023-12-24 12:41:41 +0530

    Session task

---
+++ src/components/Querry.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Querry() {
+  return (
+    <a href="https://www.npmjs.com/package/react-router-dom" target="-blank">
+      {" "}
+      Click here to redirect React router dom documentation
+    </a>
+  );
+}
+
+export default Querry;
